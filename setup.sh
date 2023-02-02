b704c24af6e23dd3048d72425ae9101535147a2f
mike0sv <mike0sv@gmail.com>
2023-02-02 17:59:47 +0600

    deployment

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install xorg -y && sudo apt-get install nvidia-driver-460 python3-pip -y && pip install torch tiktoken gitpython numpy transformers dvc-s3 mlem[flyio,fastapi] && curl -L https://fly.io/install.sh | sh && sudo reboot
 
-
-git clone https://github.com/mike0sv/nanoGPT && cd nanoGPT/ && git checkout -b mlem origin/mlem
+# git clone https://github.com/mike0sv/nanoGPT && cd nanoGPT/ && git checkout -b mlem origin/mlem
 
 # python3 data/mlem-docs/prepare.py
 
@@ -10,3 +9,6 @@ git clone https://github.com/mike0sv/nanoGPT && cd nanoGPT/ && git checkout -b m
 # python3 sample.py --out_dir=out-mlemai --dtype=float32
 
 # python3 train.py config/train_mlemai.py --device=cuda --compile=False --max_iters=3000 --init_from=scratch --dtype=float32 --dataset=wolf
+
+export FLYCTL_INSTALL="/home/ubuntu/.fly"
+export PATH="$FLYCTL_INSTALL/bin:$PATH"
